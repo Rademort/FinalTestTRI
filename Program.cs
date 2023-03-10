1672ca56e0a6af6b66a4433915bbc7f1262d5e44
Roman <r.i.tihun@gmail.com>
2023-03-10 15:03:51 +0300

    Wrote some code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длинна которых меньше либо равна 3.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+String[] ChangeArray(String[] oldArray, String[] newArray)
+{
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= 3)
+            newArray[i] = oldArray[i];
+    }
+    return newArray;
+}
+
+
+void PrintArray(String[] a)
+{
+    for (int k = 0; k < a.Length; k++)
+    {
+        System.Console.Write($"{a[k]} ");
+    }
+}
+
+
+String[] s = { "123", "asdf", "er", "3421", "qwe" };
+String[] s2 = new String[s.Length];
+
+
+ChangeArray(s, s2);
+PrintArray(s2);
